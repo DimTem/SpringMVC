d56c33929afd9b86c09bb4caa5cce86395247cea
dbrat <dmitriy_db92@mail.ru>
2017-04-09 13:27:29 +0300

    test-2

---
+++ src/main/java/com/websystique/springmvc/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package com.websystique.springmvc.security;
+
+/**
+ * Created by dbrat on 09.04.2017.
+ */
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ src/main/java/com/websystique/t2.java
@@ -0,0 +1,7 @@
+package com.websystique;
+
+/**
+ * Created by dbrat on 09.04.2017.
+ */
+public class t2 {
+}
